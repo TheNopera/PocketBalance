f351a5dcdd378828cd38810650feddd17ef95ead
Daniel Ishida <ishidadecol@gmail.com>
2023-05-12 15:37:37 -0300

    Initial Commit

---
+++ PocketBalance/PocketBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketBalanceApp.swift
+//  PocketBalance
+//
+//  Created by Daniel Ishida on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PocketBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
